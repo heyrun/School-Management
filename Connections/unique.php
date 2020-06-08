c221bfb9beb6d76a1de8ff34b5937a49e37196b2
Aaron Ebake <ebakeaaron@gmail.com>
2020-06-08 11:37:36 +0100

    Removed sensitive info

--- Connections/unique.php
+++ Connections/unique.php
@@ -2,10 +2,10 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_unique = "XpertProCombined";
-$database_unique = "uniqueblossom";
-$username_unique = "heyrun";
-$password_unique = "Journey-45";
+$hostname_unique = "Host";
+$database_unique = "DataBase Name";
+$username_unique = "test username";
+$password_unique = "Test Password";
 $unique = mysql_pconnect($hostname_unique, $username_unique, $password_unique) or trigger_error(mysql_error(),E_USER_ERROR); 
 $use_db=mysql_select_db($database_unique,$unique);
 
